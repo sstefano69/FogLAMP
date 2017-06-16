46667689362846ca11f9fd59d31ded390c5a6282
Ori Shadmon <oshadmon@gmail.com>
2017-06-15 17:23:33 -0700

    shorten import name by using "as"

--- src/python/foglamp/Python_DDL/tables.py
+++ src/python/foglamp/Python_DDL/tables.py
@@ -3,7 +3,7 @@ The following is a file containing all the tables in terms of column_name, data_
 It is intended to be used instead of a dict of tables. This file assumes that the tables already exists. 
 """
 import sqlalchemy
-import sqlalchemy.dialects
+import postgresql as postgresql 
 
 log_codes = sqlalchemy.Table('foglamp.log_codes',sqlalchemy.MetaData(),
         sqlalchemy.Column('code',sqlalchemy.CHAR(5),primary_key=True),
@@ -13,7 +13,7 @@ log = sqlalchemy.Table('foglamp.log',sqlalchemy.MetaData(),
         sqlalchemy.Column('id',sqlalchemy.BIGINT,primary_key=True),
         sqlalchemy.Column('code',sqlalchemy.CHAR(5)),
         sqlalchemy.Column('level',sqlalchemy.SMALLINT),
-        sqlalchemy.Column('log',sqlalchemy.dialects.postgresql.JSONB),
+        sqlalchemy.Column('log',postgresql.JSONB),
         sqlalchemy.Column('ts',sqlalchemy.TIMESTAMP))
 
 asset_status = sqlalchemy.Table('foglamp.asset_status',sqlalchemy.MetaData(),
@@ -29,9 +29,9 @@ assets = sqlalchemy.Table('foglamp.assets',sqlalchemy.MetaData(),
         sqlalchemy.Column('code',sqlalchemy.CHAR(50)),
         sqlalchemy.Column('description',sqlalchemy.CHAR(255)),
         sqlalchemy.Column('type_id',sqlalchemy.INTEGER),
-        sqlalchemy.Column('address',sqlalchemy.dialects.postgresql.INET), # NOT 100% this would work due to data-type
+        sqlalchemy.Column('address',postgresql.INET), # NOT 100% this would work due to data-type
         sqlalchemy.Column('status_id',sqlalchemy.INTEGER),
-        sqlalchemy.Column('properties',sqlalchemy.dialects.postgresql.JSONB),
+        sqlalchemy.Column('properties',postgresql.JSONB),
         sqlalchemy.Column('has_readings',sqlalchemy.Boolean),
         sqlalchemy.Column('ts',sqlalchemy.TIMESTAMP))
 
@@ -39,14 +39,14 @@ asset_status_changes = sqlalchemy.Table('foglamp.asset_status_changes',sqlalchem
         sqlalchemy.Column('id',sqlalchemy.BIGINT,primary_key=True),
         sqlalchemy.Column('asset_id',sqlalchemy.INTEGER),
         sqlalchemy.Column('status_id',sqlalchemy.INTEGER),
-        sqlalchemy.Column('log',sqlalchemy.dialects.postgresql.JSONB),
+        sqlalchemy.Column('log',postgresql.JSONB),
         sqlalchemy.Column('start_ts',sqlalchemy.TIMESTAMP),
         sqlalchemy.Column('ts',sqlalchemy.TIMESTAMP))
 
 links = sqlalchemy.Table('foglmap.links',sqlalchemy.MetaData(),
         sqlalchemy.Column('id',sqlalchemy.INTEGER),
         sqlalchemy.Column('asset_id',sqlalchemy.INTEGER),
-        sqlalchemy.Column('properties',sqlalchemy.dialects.postgresql.JSONB),
+        sqlalchemy.Column('properties',postgresql.JSONB),
         sqlalchemy.Column('ts',sqlalchemy.TIMESTAMP))
 
 asset_links = sqlalchemy.Table('foglamp.asset_links',sqlalchemy.MetaData(),
@@ -62,31 +62,31 @@ asset_messages = sqlalchemy.Table('foglamp.asset_messages',sqlalchemy.MetaData()
         sqlalchemy.Column('id',sqlalchemy.BIGINT,primary_key=True),
         sqlalchemy.Column('asset_id',sqlalchemy.INTEGER),
         sqlalchemy.Column('status_id',sqlalchemy.INTEGER),
-        sqlalchemy.Column('message',sqlalchemy.dialects.postgresql.JSONB),
+        sqlalchemy.Column('message',postgresql.JSONB),
         sqlalchemy.Column('ts',sqlalchemy.TIMESTAMP))
 
 
 readings = sqlalchemy.Table('foglamp.readings',sqlalchemy.MetaData(),
         sqlalchemy.Column('id',sqlalchemy.BIGINT, primary_key=True),
         sqlalchemy.Column('asset_code',sqlalchemy.CHAR(50)),
-        sqlalchemy.Column('read_key',sqlalchemy.dialects.postgresql.UUID), # NOT 100% this would work due to data-type
-        sqlalchemy.Column('reading',sqlalchemy.dialects.postgresql.JSONB),
+        sqlalchemy.Column('read_key',postgresql.UUID), # NOT 100% this would work due to data-type
+        sqlalchemy.Column('reading',postgresql.JSONB),
         sqlalchemy.Column('user_ts',sqlalchemy.TIMESTAMP),
         sqlalchemy.Column('ts',sqlalchemy.TIMESTAMP))
 
 destinations = sqlalchemy.Table('foglamp.destinations',sqlalchemy.MetaData(),
         sqlalchemy.Column('id',sqlalchemy.INTEGER,primary_key=True),
         sqlalchemy.Column('description',sqlalchemy.CHAR(255)),
-        sqlalchemy.Column('properties',sqlalchemy.dialects.postgresql.JSONB),
-        sqlalchemy.Column('address',sqlalchemy.dialects.postgresql.INET),
+        sqlalchemy.Column('properties',postgresql.JSONB),
+        sqlalchemy.Column('address',postgresql.INET),
         sqlalchemy.Column('ts',sqlalchemy.TIMESTAMP))
 
 streams = sqlalchemy.Table('foglamp.streams',sqlalchemy.MetaData(),
         sqlalchemy.Column('id',sqlalchemy.INTEGER,primary_key=True),
         sqlalchemy.Column('destination_id',sqlalchemy.INTEGER),
         sqlalchemy.Column('description',sqlalchemy.CHAR(255)),
-        sqlalchemy.Column('properties',sqlalchemy.dialects.postgresql.JSONB),
-        sqlalchemy.Column('filters',sqlalchemy.dialects.postgresql.JSONB),
+        sqlalchemy.Column('properties',postgresql.JSONB),
+        sqlalchemy.Column('filters',postgresql.JSONB),
         sqlalchemy.Column('reading_id',sqlalchemy.BIGINT),
         sqlalchemy.Column('ts',sqlalchemy.TIMESTAMP))
 
@@ -103,34 +103,34 @@ stream_log = sqlalchemy.Table('foglamp.stream_log',sqlalchemy.MetaData(),
         sqlalchemy.Column('stream_id',sqlalchemy.INTEGER),
         sqlalchemy.Column('status_id',sqlalchemy.INTEGER),
         sqlalchemy.Column('reading_id',sqlalchemy.INTEGER),
-        sqlalchemy.Column('log',sqlalchemy.dialects.postgresql.JSONB),
+        sqlalchemy.Column('log',postgresql.JSONB),
         sqlalchemy.Column('ts',sqlalchemy.TIMESTAMP))
 
 configuration = sqlalchemy.Table('foglamp.configuration',sqlalchemy.MetaData(),
         sqlalchemy.Column('key',sqlalchemy.CHAR(5), primary_key=True),
-        sqlalchemy.Column('value',sqlalchemy.dialects.postgresql.JSONB),
+        sqlalchemy.Column('value',postgresql.JSONB),
         sqlalchemy.Column('ts',sqlalchemy.TIMESTAMP))
 
 configuration_changes = sqlalchemy.Table('foglamp.configuration_changes',sqlalchemy.MetaData(),
         sqlalchemy.Column('key',sqlalchemy.CHAR(5)),
         sqlalchemy.Column('configuration_ts',sqlalchemy.TIMESTAMP),
-        sqlalchemy.Column('configuration_value',sqlalchemy.dialects.postgresql.JSONB),
+        sqlalchemy.Column('configuration_value',postgresql.JSONB),
         sqlalchemy.Column('ts',sqlalchemy.TIMESTAMP))
 
 clean_rules = sqlalchemy.Table('foglamp.clean_rules',sqlalchemy.MetaData(),
         sqlalchemy.Column('id',sqlalchemy.INTEGER, primary_key=True),
         sqlalchemy.Column('type',sqlalchemy.CHAR(3)),
         sqlalchemy.Column('object_id',sqlalchemy.BIGINT),
-        sqlalchemy.Column('rule',sqlalchemy.dialects.postgresql.JSONB),
-        sqlalchemy.Column('rule_check',sqlalchemy.dialects.postgresql.JSONB),
+        sqlalchemy.Column('rule',postgresql.JSONB),
+        sqlalchemy.Column('rule_check',postgresql.JSONB),
         sqlalchemy.Column('status',sqlalchemy.SMALLINT),
         sqlalchemy.Column('ts',sqlalchemy.TIMESTAMP))
 
 cloud_send_rules = sqlalchemy.Table('foglamp.cloud_send_rules',sqlalchemy.MetaData(),
         sqlalchemy.Column('id',sqlalchemy.INTEGER,primary_key=True),
         sqlalchemy.Column('stream_id',sqlalchemy.INTEGER),
-        sqlalchemy.Column('rule',sqlalchemy.dialects.postgresql.JSONB),
-        sqlalchemy.Column('rule_check',sqlalchemy.dialects.postgresql.JSONB),
+        sqlalchemy.Column('rule',postgresql.JSONB),
+        sqlalchemy.Column('rule_check',postgresql.JSONB),
         sqlalchemy.Column('ts',sqlalchemy.TIMESTAMP))
 
 resources = sqlalchemy.Table('foglamp.resources',sqlalchemy.MetaData(),
@@ -146,12 +146,12 @@ roles = sqlalchemy.Table('foglamp.roles',sqlalchemy.MetaData(),
 role_resource_permission = sqlalchemy.Table('foglamp.role_resource_permission', sqlalchemy.MetaData(),
         sqlalchemy.Column('role_id',sqlalchemy.INTEGER,primary_key=True),
         sqlalchemy.Column('resource_id',sqlalchemy.INTEGER,primary_key=True),
-        sqlalchemy.Column('access',sqlalchemy.dialects.postgresql.JSONB))
+        sqlalchemy.Column('access',postgresql.JSONB))
 
 role_asset_permissions = sqlalchemy.Table('foglamp.role_asset_permissions',sqlalchemy.MetaData(),
         sqlalchemy.Column('role_ide',sqlalchemy.INTEGER,primary_key=True),
         sqlalchemy.Column('asset_id',sqlalchemy.INTEGER,primary_key=True),
-        sqlalchemy.Column('access',sqlalchemy.dialects.postgresql.JSONB))
+        sqlalchemy.Column('access',postgresql.JSONB))
 
 users = sqlalchemy.Table('foglamp.users',sqlalchemy.MetaData(),
         sqlalchemy.Column("id",sqlalchemy.INTEGER,primary_key=True),
@@ -165,15 +165,15 @@ users = sqlalchemy.Table('foglamp.users',sqlalchemy.MetaData(),
 user_logins = sqlalchemy.Table('foglamp.user_logins',sqlalchemy.MetaData(),
         sqlalchemy.Column('id',sqlalchemy.INTEGER,primary_key=True),
         sqlalchemy.Column('user_id',sqlalchemy.INTEGER),
-        sqlalchemy.Column('ip',sqlalchemy.dialects.postgresql.INET),
+        sqlalchemy.Column('ip',postgresql.INET),
         sqlalchemy.Column('ts',sqlalchemy.TIMESTAMP))
 
 user_resource_permissions = sqlalchemy.Table('foglamp.user_resource_permissions',sqlalchemy.MetaData(),
         sqlalchemy.Column('user_id',sqlalchemy.INTEGER,primary_key=True),
         sqlalchemy.Column('resource_id',sqlalchemy.INTEGER,primary_key=True),
-        sqlalchemy.Column('access',sqlalchemy.dialects.postgresql.JSONB))
+        sqlalchemy.Column('access',postgresql.JSONB))
 
 user_asset_permissions = sqlalchemy.Table('foglamp.user_asset_permissions',sqlalchemy.MetaData(),
         sqlalchemy.Column('user_id',sqlalchemy.INTEGER,primary_key=True),
         sqlalchemy.Column('asset_id',sqlalchemy.INTEGER,primary_key=True),
-        sqlalchemy.Column('access',sqlalchemy.dialects.postgresql.JSONB))
+        sqlalchemy.Column('access',postgresql.JSONB))
