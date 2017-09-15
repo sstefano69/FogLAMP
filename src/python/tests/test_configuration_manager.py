85620ee1a54fc9a2a2e85854581608f1359a5da3
Ori Shadmon <oshadmon@gmail.com>
2017-09-15 21:42:47 +0100

    Added testing for configuration_manager.create_category

--- src/python/tests/test_configuration_manager.py
+++ src/python/tests/test_configuration_manager.py
@@ -1,6 +1,5 @@
-from foglamp.configuration_manager import (create_category, _configuration_tbl, get_all_category_names,
-                                           get_category_all_items, get_category_item_value_entry,
-                                           get_category_item_value_entry)
+"""The following tests the configuration manager component"""
+from foglamp.configuration_manager import (create_category, _configuration_tbl)
 import aiopg
 import pytest
 import sqlalchemy as sa
@@ -10,25 +9,29 @@ __copyright__ = "Copyright (c) 2017 OSIsoft, LLC"
 __license__ = "Apache 2.0"
 __version__ = "${VERSION}"
 
-_connection_string = "dbname='foglamp'"
+_CONNECTION_STRING = "dbname='foglamp'"
 
 
+@pytest.fixture(scope="module")
 async def _delete_from_configuration():
     try:
-        async with aiopg.sa.create_engine(_connection_string) as engine:
+        async with aiopg.sa.create_engine(_CONNECTION_STRING) as engine:
             async with engine.acquire() as conn:
                 await conn.execute("DELETE FROM foglamp.configuration")
     except Exception:
         print("DELETE Failed")
         raise
 
+
+@pytest.fixture(scope="module")
 async def _new_category()->dict:
     """
     INSERT information found in data dictionary into configuration table
     :return:
         data: A dictionary with hardcoded values in configuration table
             data = {key:{description:str, value:json}}
-                json={info:{description:str, type:str(data_type}, default:str(data_type_value)}}
+                json={info:{description:str, type:str(data_type},
+                      default:str(data_type_value)}}
     """
     data = {
         'boolean': {'category_description': 'boolean type',
@@ -80,24 +83,30 @@ async def _new_category()->dict:
                              'type': 'password',
                              'default': ''}}}
     }
-    for name in data.keys():
-        await create_category(category_name=name, category_description=data[name]['category_description'],
+    for name in data:
+        await create_category(category_name=name,
+                              category_description=data[name]['category_description'],
                               category_value=data[name]['category_value'])
     return data
 
+
 @pytest.mark.asyncio
+@pytest.allure.feature("unit")
+@pytest.allure.story("configuration_manager")
 async def test_new_category():
     """
-    Test the _delete_from_configuration() and _new_category() methods that are used in consequent tests
+    Test the _delete_from_configuration() and _new_category() methods that are used in
+    consequent tests behave as expected
     :assert:
         1. No data exists in configuration
         2. All data was inserted into configuration
-        3. Each column in configuration table contains valid row information for each category
+        3. Each column in configuration table contains valid row information for
+        each category
     """
     await _delete_from_configuration()
     stmt = sa.select([sa.func.count()]).select_from(_configuration_tbl)
     try:
-        async with aiopg.sa.create_engine(_connection_string) as engine:
+        async with aiopg.sa.create_engine(_CONNECTION_STRING) as engine:
             async with engine.acquire() as conn:
                 async for count in conn.execute(stmt):
                     assert count[0] == 0
@@ -107,7 +116,7 @@ async def test_new_category():
 
     data = await _new_category()
     try:
-        async with aiopg.sa.create_engine(_connection_string) as engine:
+        async with aiopg.sa.create_engine(_CONNECTION_STRING) as engine:
             async with engine.acquire() as conn:
                 async for count in conn.execute(stmt):
                     assert count[0] == len(list(data.keys()))
@@ -116,25 +125,112 @@ async def test_new_category():
         raise
     for key in data.keys():
         stmt = sa.select([_configuration_tbl.c.key, _configuration_tbl.c.description,
-                          _configuration_tbl.c.value]).select_from(_configuration_tbl).where(
-            _configuration_tbl.c.key == key)
+                          _configuration_tbl.c.value]).select_from(
+                              _configuration_tbl).where(
+                                  _configuration_tbl.c.key == key)
         try:
-            async with aiopg.sa.create_engine(_connection_string) as engine:
+            async with aiopg.sa.create_engine(_CONNECTION_STRING) as engine:
                 async with engine.acquire() as conn:
                     async for result in conn.execute(stmt):
-                        assert result[0].replace(" ","") == key
+                        assert result[0].replace(" ", "") == key
                         assert result[1] == data[key]['category_description']
                         # category_value dictionary assert
                         assert list(result[2].keys())[0] == "info"
-                        assert result[2]['info']['description'] == data[key]['category_value']['info']['description']
-                        assert result[2]['info']['type'] == data[key]['category_value']['info']['type']
-                        assert result[2]['info']['default'] == data[key]['category_value']['info']['default']
+                        assert result[2]['info']['description'] == data[key][
+                            'category_value']['info']['description']
+                        assert result[2]['info']['type'] == data[key]['category_value'][
+                            'info']['type']
+                        assert result[2]['info']['default'] == data[key]['category_value'][
+                            'info']['default']
         except Exception:
             print("QUERY Failed")
             raise
     await _delete_from_configuration()
 
+
 @pytest.mark.asyncio
+@pytest.allure.feature("unit")
+@pytest.allure.story("configuration_manager")
+async def test_new_category_original_items_true():
+    """
+    Test the behavior of create_category when keep_original_items == True
+    :assert:
+        1. `values` dictionary has both categories
+        2. values in 'data' category are as expected
+        3. values in 'info' category did not change
+    """
+    stmt = sa.select([_configuration_tbl.c.value]).select_from(_configuration_tbl).where(
+        _configuration_tbl.c.key == 'boolean')
+    await _delete_from_configuration()
+    data = await _new_category()
+    await create_category(category_name='boolean',
+                          category_description=data['boolean']['category_description'],
+                          category_value={'data': {
+                              'description': 'boolean type with default True',
+                              'type': 'boolean',
+                              'default': 'True'}},
+                          keep_original_items=True)
+    try:
+        async with aiopg.sa.create_engine(_CONNECTION_STRING) as engine:
+            async with engine.acquire() as conn:
+                async for result in conn.execute(stmt):
+                    assert sorted(list(result[0].keys())) == ['data', 'info']
+                    assert result[0]['data']['description'] == (
+                        'boolean type with default True')
+                    assert result[0]['data']['type'] == 'boolean'
+                    assert result[0]['data']['default'] == 'True'
+                    assert result[0]['info']['description'] == (
+                        data['boolean']['category_value']['info']['description'])
+                    assert result[0]['info']['type'] == (
+                        data['boolean']['category_value']['info']['type'])
+                    assert result[0]['info']['default'] == (
+                        data['boolean']['category_value']['info']['default'])
+    except Exception:
+        print('Query failed')
+        raise
+    await _delete_from_configuration
+
+
+@pytest.mark.asyncio
+@pytest.allure.feature("unit")
+@pytest.allure.story("configuration_manager")
+async def test_new_category_original_items_false():
+    """
+    Test the behavior of create_category when keep_original_items == False
+    :assert:
+        1. `values` dictionary only has 'data' category
+        2. values in 'data' category are as expected
+    """
+    stmt = sa.select([_configuration_tbl.c.value]).select_from(_configuration_tbl).where(
+        _configuration_tbl.c.key == 'boolean')
+    await _delete_from_configuration()
+    data = await _new_category()
+    await create_category(category_name='boolean',
+                          category_description=data['boolean']['category_description'],
+                          category_value={'data': {
+                              'description': 'boolean type with default True',
+                              'type': 'boolean',
+                              'default': 'True'}},
+                          keep_original_items=False)
+    try:
+        async with aiopg.sa.create_engine(_CONNECTION_STRING) as engine:
+            async with engine.acquire() as conn:
+                async for result in conn.execute(stmt):
+                    assert sorted(list(result[0].keys())) == ['data']
+                    assert result[0]['data']['description'] == (
+                        'boolean type with default True')
+                    assert result[0]['data']['type'] == 'boolean'
+                    assert result[0]['data']['default'] == 'True'
+
+    except Exception:
+        print('Query failed')
+        raise
+    await _delete_from_configuration
+
+
+@pytest.mark.asyncio
+@pytest.allure.feature("unit")
+@pytest.allure.story("configuration_manager")
 async def test_create_category_error():
     """
     Test that ValueError is returned when category_value.type is invalid
@@ -147,59 +243,22 @@ async def test_create_category_error():
     with pytest.raises(ValueError) as error_exec:
         await create_category(category_name='float', category_description='float type',
                               category_value={
-                                'info': {
-                                    'description': 'float type with default 1.1',
-                                    'type': 'float',
-                                    'default': '1.1'}
-                            }
-                            )
+                                  'info': {
+                                      'description': 'float type with default 1.1',
+                                      'type': 'float',
+                                      'default': '1.1'}})
     assert 'ValueError' in str(error_exec)
-    assert "['boolean', 'integer', 'string', 'IPv4', 'IPv6', 'X509 certificate', 'password', 'JSON']" in str(error_exec)
+    assert ("['boolean', 'integer', 'string', 'IPv4', 'IPv6', 'X509 certificate', "
+            + "'password', 'JSON']") in str(error_exec)
 
     stmt = sa.select([sa.func.count()]).select_from(_configuration_tbl)
     try:
-        async with aiopg.sa.create_engine(_connection_string) as engine:
+        async with aiopg.sa.create_engine(_CONNECTION_STRING) as engine:
             async with engine.acquire() as conn:
                 async for count in conn.execute(stmt):
                     assert count[0] == 0
     except Exception:
         print("QUERY Failed")
         raise
-
-
     await _delete_from_configuration()
 
-@pytest.mark.asyncio
-async def test_get_all_category_names():
-    """
-    Test that the names and descriptions of all categories are returned
-    :assert:
-        1. Nothing gets returned since there isn't data in the table
-        2. The number of sets returned is equal to the number of keys in data
-        3. The description in data (based on key/category_description) is the same as the description in result
-    """
-    await _delete_from_configuration()
-    results = await get_all_category_names()
-    assert results == []
-    data = await _new_category()
-    results = await get_all_category_names()
-    assert len(results) == len(data.keys())
-    for result in results:
-        assert data[result[0].replace(" ", "")]['category_description'] == result[1]
-    await _delete_from_configuration()
-
-@pytest.mark.asyncio
-async def test_get_category_all_items():
-    """
-    Test get_category_all_items() returns data[key]['category_value'] for each key
-    :assert:
-        The info values returned correspond to the value in data
-    """
-    await _delete_from_configuration()
-    data = await _new_category()
-    for key in data.keys():
-        result = await get_category_all_items(key)
-        assert data[key]['category_value']['info']['description'] == result['info']['description']
-        assert data[key]['category_value']['info']['type'] == result['info']['type']
-        assert data[key]['category_value']['info']['default'] == result['info']['default']
-    await _delete_from_configuration()
