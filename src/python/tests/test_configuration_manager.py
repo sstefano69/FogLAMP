700afa839be9f48427d18c1bfc878e611c111ee3
Ori Shadmon <oshadmon@gmail.com>
2017-09-18 21:17:37 +0100

    with the exception of register_interest testing for all other methods in configuration_manager is done

--- src/python/tests/test_configuration_manager.py
+++ src/python/tests/test_configuration_manager.py
@@ -1,6 +1,12 @@
-"""The following tests the configuration manager component"""
+"""
+The following tests the configuration manager component For the most part,
+the code uses the boolean type for testing due to simplicity; but contains
+tests to verify which data_types are supported and which are not.
+"""
 from foglamp.configuration_manager import (create_category, _configuration_tbl,
-                                           register_interest, set_category_item_value_entry)
+                                           set_category_item_value_entry, get_category_item,
+                                           get_category_item_value_entry, get_category_all_items,
+                                           get_all_category_names)
 import aiopg
 import pytest
 import sqlalchemy as sa
@@ -26,7 +32,7 @@ async def _delete_from_configuration():
 @pytest.mark.asyncio
 @pytest.allure.feature("unit")
 @pytest.allure.story("configuration_manager")
-async def test_new_category_default_data_types():
+async def test_create_category_default_data_types():
     """
     Test the _delete_from_configuration() method works properly, and that the expected
     (supported) data types do get inserted without an error.
@@ -146,7 +152,7 @@ async def test_new_category_default_data_types():
 @pytest.mark.asyncio
 @pytest.allure.feature("unit")
 @pytest.allure.story("configuration_manager")
-async def test_new_category_keep_original_items_true():
+async def test_create_category_keep_original_items_true():
     """
     Test the behavior of create_category when keep_original_items == True
     :assert:
@@ -195,7 +201,7 @@ async def test_new_category_keep_original_items_true():
 @pytest.mark.asyncio
 @pytest.allure.feature("unit")
 @pytest.allure.story("configuration_manager")
-async def test_new_category_keep_original_items_false():
+async def test_create_category_keep_original_items_false():
     """
     Test the behavior of create_category when keep_original_items == False
     :assert:
@@ -205,13 +211,6 @@ async def test_new_category_keep_original_items_false():
     stmt = sa.select([_configuration_tbl.c.value]).select_from(_configuration_tbl).where(
         _configuration_tbl.c.key == 'boolean')
     await _delete_from_configuration()
-
-    await create_category(category_name='boolean', category_description='boolean type',
-                          category_value={
-                              'info': {
-                                  'description': 'boolean type with default False',
-                                  'type': 'boolean',
-                                  'default': 'False'}})
 
     await create_category(category_name='boolean',
                           category_description='boolean type',
@@ -244,8 +243,11 @@ async def test_create_category_errors():
     """
     Test that create_category returns the expected error for each case
     :assert:
-        1. Assert that TypeError gets returned when  type is invalid (used 'float)
+        1. Assert that TypeError gets returned when  type is invalid (used 'float')
         2. Asert that ValueError gets returned when entry_name type is missing from item_name
+        3. Assert that TypeError gets returned when entry_name type is None
+        4. Assert that TypeError gets returned when entry_name type is an actual value,
+            rather than the string of it (specifically Boolean and Integer types)
     """
     await _delete_from_configuration()
     with pytest.raises(ValueError) as error_exec:
@@ -286,30 +288,45 @@ async def test_create_category_errors():
                                       'default': 'integer type with value 1'}})
     assert "ValueError: Missing entry_name description for item_name info" in str(error_exec)
 
-    await _delete_from_configuration()
+    with pytest.raises(TypeError) as error_exec:
+        await create_category(category_name='boolean',
+                              category_description='boolean type',
+                              category_value={'data': {
+                                  'description': 'boolean type with default True',
+                                  'type': 'boolean',
+                                  'default': None}},
+                              keep_original_items=False)
+    assert "TypeError: entry_val must be a string for item_name data and entry_name default" in str(error_exec)
 
+    with pytest.raises(TypeError) as error_exec:
+        await create_category(category_name='boolean',
+                              category_description='boolean type',
+                              category_value={'data': {
+                                  'description': 'boolean type with default True',
+                                  'type': None,
+                                  'default': 'False'}},
+                              keep_original_items=False)
+    assert "TypeError: entry_val must be a string for item_name data and entry_name type" in str(error_exec)
 
-@pytest.mark.asyncio
-@pytest.allure.feature("unit")
-@pytest.allure.story("configuration_manager")
-async def test_register_interest_errors():
-    """
-    Test that appropriate errors are returned for register_interest
-    :assert:
-        1. An error is returned when category_name = None
-        2. An error is retund when callback = None
-    """
-    await _delete_from_configuration()
-    with pytest.raises(ValueError) as error_exec:
-        register_interest(category_name=None, callback='foglamp.callback')
-    assert 'ValueError: Failed to register interest. category_name cannot be None' in (
-        str(error_exec))
+    with pytest.raises(TypeError) as error_exec:
+        await create_category(category_name='boolean',
+                          category_description='boolean type',
+                          category_value={'data': {
+                              'description': 'boolean type with default True',
+                              'type': 'boolean',
+                              'default': False}},
+                          keep_original_items=False)
+    assert "TypeError: entry_val must be a string for item_name data and entry_name default" in str(error_exec)
 
-    await _delete_from_configuration()
-    with pytest.raises(ValueError) as error_exec:
-        register_interest(category_name='boolean', callback=None)
-    assert 'ValueError: Failed to register interest. callback cannot be None' in (
-        str(error_exec))
+    with pytest.raises(TypeError) as error_exec:
+        await create_category(category_name='integer',
+                          category_description='integer type',
+                          category_value={'data': {
+                              'description': 'integers type with default o',
+                              'type': 'integer',
+                              'default': 0}},
+                          keep_original_items=False)
+    assert "TypeError: entry_val must be a string for item_name data and entry_name default" in str(error_exec)
     await _delete_from_configuration()
 
 
@@ -342,6 +359,7 @@ async def test_set_category_item_value_entry():
     except Exception:
         print('Query failed: %s' % stmt)
         raise
+
     await set_category_item_value_entry(category_name='boolean',
                                         item_name='info', new_value_entry='True')
     try:
@@ -353,4 +371,237 @@ async def test_set_category_item_value_entry():
     except Exception:
         print('Query failed: %s' % stmt)
         raise
+    await _delete_from_configuration()
+
+@pytest.mark.asyncio
+@pytest.allure.feature("unit")
+@pytest.allure.feature("configuration_manager")
+async def test_get_category_item_value_entry():
+    """
+    Test that get_category_item_value_entry works properly
+    :Assert:
+        1. category_value.value gets returned and matchates default
+        2. When updating value, the data retrieved for value gets updated
+    """
+    await _delete_from_configuration()
+    await create_category(category_name='boolean', category_description='boolean type',
+                          category_value={
+                              'info': {
+                                  'description': 'boolean type with default False',
+                                  'type': 'boolean',
+                                  'default': 'False'}
+                          })
+    result = await get_category_item_value_entry(category_name='boolean', item_name='info')
+    assert result == 'False'
+
+    await set_category_item_value_entry(category_name='boolean',
+                                        item_name='info', new_value_entry='True')
+    result = await get_category_item_value_entry(category_name='boolean', item_name='info')
+    assert result == 'True'
+    await _delete_from_configuration()
+
+@pytest.mark.asyncio
+@pytest.allure.feature("unit")
+@pytest.allure.feature("configuration_manager")
+async def test_get_category_item_value_entry_empty():
+    """
+    Test that None gets returned when either category_name and/or item_name don't exist
+    :assert:
+        1. Assert None is returned when item_name is does not exist
+        2. Assert None is returned when category_name does not exist
+    """
+    await _delete_from_configuration()
+    await create_category(category_name='boolean', category_description='boolean type',
+                          category_value={
+                              'info': {
+                                  'description': 'boolean type with default False',
+                                  'type': 'boolean',
+                                  'default': 'False'}
+                          })
+    result = await get_category_item_value_entry(category_name='boolean', item_name='data')
+    assert result is None
+
+    result = await get_category_item_value_entry(category_name='integer', item_name='info')
+    assert result is None
+    await _delete_from_configuration()
+
+
+@pytest.mark.asyncio
+@pytest.allure.feature("unit")
+@pytest.allure.feature("configuration_manager")
+async def test_get_category_item():
+    """
+    Test that gt_category_item returns all the data in configuration.value for a specific category_name
+    :assert:
+        1. Information in configuration.value match the category_values declared
+        2. When updating value, the data retrieved for default is as expected
+    """
+    await _delete_from_configuration()
+    await create_category(category_name='boolean', category_description='boolean type',
+                          category_value={
+                              'info': {
+                                  'description': 'boolean type with default False',
+                                  'type': 'boolean',
+                                  'default': 'False'}
+                          })
+    result = await get_category_item(category_name='boolean', item_name='info')
+    assert result['description'] == 'boolean type with default False'
+    assert result['type'] == 'boolean'
+    assert result['default'] == 'False'
+    assert result['value'] == 'False'
+
+    await set_category_item_value_entry(category_name='boolean',
+                                        item_name='info', new_value_entry='True')
+    result = await get_category_item(category_name='boolean', item_name='info')
+    assert result['default'] == 'False'
+    assert result['value'] == 'True'
+
+    await _delete_from_configuration()
+
+@pytest.mark.asyncio
+@pytest.allure.feature("unit")
+@pytest.allure.feature("configuration_manager")
+async def test_get_category_item_empty():
+    """
+    Test that gt_category_item when either category_name or item_name do not exist
+    :assert:
+        Assert result is None when category_name or item_name do not exist in configuration
+    """
+    await _delete_from_configuration()
+    await create_category(category_name='boolean', category_description='boolean type',
+                          category_value={
+                              'info': {
+                                  'description': 'boolean type with default False',
+                                  'type': 'boolean',
+                                  'default': 'False'}
+                          })
+    result = await get_category_item(category_name='integer', item_name='info')
+    assert result is None
+
+    result = await get_category_item(category_name='boolean', item_name='data')
+    assert result is None
+    await _delete_from_configuration()
+
+@pytest.mark.asyncio
+@pytest.allure.feature("unit")
+@pytest.allure.feature("configuration_manager")
+async def test_get_category_all_items():
+    """
+    Test get_category_all_items method returns full "dictionary" of category_value
+    :assert:
+        1.  Values in dictionary are as expcted
+        2. default doesn't get updated when value does
+    """
+    await  _delete_from_configuration()
+    await create_category(category_name='boolean', category_description='boolean type',
+                          category_value={
+                              'info': {
+                                  'description': 'boolean type with default False',
+                                  'type': 'boolean',
+                                  'default': 'False'}
+                          })
+
+    result = await get_category_all_items(category_name='boolean')
+    assert result['info']['description'] == 'boolean type with default False'
+    assert result['info']['type'] == 'boolean'
+    assert result['info']['default'] == 'False'
+    assert result['info']['value'] == 'False'
+
+    await  set_category_item_value_entry(category_name='boolean', item_name='info', new_value_entry='True')
+    result = await get_category_all_items(category_name='boolean')
+    assert result['info']['default'] == 'False'
+    assert result['info']['value'] == 'True'
+
+    await _delete_from_configuration()
+
+@pytest.mark.asyncio
+@pytest.allure.feature("unit")
+@pytest.allure.feature("configuration_manager")
+async def test_get_category_all_items_empty():
+    """
+    Test get_category_all_items doesn't return anything if category_name doesn't exist
+    :assert:
+        Assert None gets returned when category_name does not exist
+    """
+    await  _delete_from_configuration()
+    await create_category(category_name='boolean', category_description='boolean type',
+                          category_value={
+                              'info': {
+                                  'description': 'boolean type with default False',
+                                  'type': 'boolean',
+                                  'default': 'False'}
+                          })
+
+    result = await get_category_all_items(category_name='integer')
+    assert result is None
+    await _delete_from_configuration()
+
+@pytest.mark.asyncio
+@pytest.allure.feature("unit")
+@pytest.allure.feature("configuration_manager")
+async def test_get_all_category_names():
+    """
+    Test get_all_category_names validly returns the category_name and category_description
+    :assert:
+        Assert that the category_name retrieved corresponds to the category_description with the use
+        of the existing dictionary (`data`)
+    """
+    await _delete_from_configuration()
+    data = {
+        'boolean': {'category_description': 'boolean type',
+                    'category_value': {
+                        'info': {
+                            'description': 'boolean type with default False',
+                            'type': 'boolean',
+                            'default': 'False'}}},
+        'integer': {'category_description': 'integer type',
+                    'category_value': {
+                        'info': {
+                            'description': 'integer type with default 1',
+                            'type': 'integer',
+                            'default': '1'}}},
+        'string': {'category_description': 'string type',
+                   'category_value': {
+                       'info': {
+                           'description': "string type with default 'ABCabc'",
+                           'type': 'string',
+                           'default': 'ABCabc'}}},
+        'JSON': {'category_description': 'JSON type',
+                 'category_value': {
+                     'info': {
+                         'description': "JSON type with default {}",
+                         'type': 'JSON',
+                         'default': '{}'}}},
+        'IPv4': {'category_description': 'IPv4 type',
+                 'category_value': {
+                     'info': {
+                         'description': "IPv4 type with default '127.0.0.1'",
+                         'type': 'IPv4',
+                         'default': '127.0.0.1'}}},
+        'IPv6': {'category_description': 'IPv6 type',
+                 'category_value': {
+                     'info': {
+                         'description': "IPv6 type with default '2001:db8::'",
+                         'type': 'IPv6',
+                         'default': '2001:db8::'}}},
+        'X509': {'category_description': 'X509 Certification',
+                 'category_value': {
+                     'info': {
+                         'description': "X509 Certification",
+                         'type': 'X509 certificate',
+                         'default': 'x509_certificate.cer'}}},
+        'password': {'category_description': 'Password Type',
+                     'category_value': {
+                         'info': {
+                             'description': "Password Type with default ''",
+                             'type': 'password',
+                             'default': ''}}}
+    }
+    for category_name in data:
+        await create_category(category_name=category_name,
+                              category_description=data[category_name]['category_description'],
+                              category_value=data[category_name]['category_value'])
+    results = await get_all_category_names()
+    for result in results:
+        assert data[result[0].replace(" ","")]['category_description'] == result[1]
     await _delete_from_configuration()
